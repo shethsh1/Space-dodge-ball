92ef9b4a9656cc20cd4ffc0b22cb14dedeff24ca
shethsh1 <shaahid.sheth@mail.utoronto.ca>
2021-03-30 00:01:38 -0400

    Added score testing

--- testingfile.asm
+++ testingfile.asm
@@ -15,7 +15,13 @@
 	Alphabet_V:	.word	1448, 1320, 1576, 1708, 1840, 1716, 1592, 1464, 1336
 	Alphabet_E_2:	.word	1348, 1352, 1356,1360,1364, 1476, 1604, 1608, 1612,1616, 1732, 1860, 1864,1868,1872,1876
 	Alphabet_R:	.word	1888, 1760, 1632, 1504, 1376, 1380, 1384, 1388, 1520, 1648, 1644,1772, 1904, 1640, 1636
+	S2:		.word	3232, 3104, 2976, 2848, 2724, 2728, 2860, 2988, 3116, 3244, 3368, 3364
+	S1:		.word	3256, 3128, 3000, 2872, 2748, 2752, 2884, 3012, 3140, 3268, 3392, 3388
+	S0:		.word	3280, 3152, 3024, 2896, 2772, 2776, 2908, 3036, 3164, 3292, 3416, 3412
+	number_zero:	.word	3232, 3104, 2976, 2848, 2724, 2728, 2860, 2988, 3116, 3244, 3368, 3364
 .text
+
+	# + 24 diff, s2 is the most left digit 
 
 	li $t0, BASE_ADDRESS # $t0 stores the base address for display
 	li $t1, 0xd3d3d3 # $t1 stores the grey colour code
@@ -142,10 +148,34 @@
 			addi $a0, $a0, 1
 			bne $t5, 1636, create_R		# change, + number
 
-			j done2				# change
+			j SCORE				# change
 			
-		done2:
+SCORE:
 			
+	la $t4, S2	# $t4 = addr(zero)
+	li $t5, 0		# t5 = 0
+	j CREATE_SCORE
+
+CREATE_SCORE:
+	sll $a0, $t5, 2		# a0 = t5 x 2
+	add $t6, $t4, $a0	# t6 = addr(t4 + a0)
+	lw $t7, 0($t6)		# a0 = t4[i]
+	add $a0, $t7, $t0	# map + a0 
+	sw $t8, 0($a0)
+
+	add $t5, $t5, 1
+	bne $t7, 3364, CREATE_SCORE
+	la $t4, S1	# $t4 = addr(zero)
+	li $t5, 0		# t5 = 0
+	bne $t7, 3388, CREATE_SCORE
+	la $t4, S0	# $t4 = addr(zero)
+	li $t5, 0		# t5 = 0
+	j CREATE_SCORE
+	
+
+
+
+	
 			
 		
 			
